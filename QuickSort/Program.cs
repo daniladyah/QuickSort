2a032656d8696d2e943923cea036c704703c3e5f
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2022-10-17 18:01:29 +0700

    lalu exit from the console

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -131,6 +131,8 @@ namespace QuickSort
             //Frist call to Quick sort Alogarithm
             myList.q_sort(0, myList.getSize() - 1);
             //Display sorted array
+            myList.display();
+            // to exit from the console
             
 
 
