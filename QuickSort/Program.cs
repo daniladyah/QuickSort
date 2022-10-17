f5c8ba9eda6c2fe0c4effe2070f79985aac65a7b
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2022-10-17 16:59:25 +0700

    membuat class program

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -9,7 +9,15 @@ namespace QuickSort
     class Program
     {
         //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0; // number of data movements
 
+        // Number of elements in arrray
+        private int n;
+
+
+        void read ()
         {
         }
     }
