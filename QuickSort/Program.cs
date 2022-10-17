e65b373ab2b9d965d1820f8d4db5ba4c544e0a75
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2022-10-17 17:19:44 +0700

    menambahkan swaps the elements at index x with thw element at index y

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -17,7 +17,7 @@ namespace QuickSort
         private int n;
 
 
-        void read ()
+        void read()
         {
             while (true)
             {
@@ -34,11 +34,14 @@ namespace QuickSort
             Console.WriteLine("\n======================");
 
             //get array elements
-            for(int i = 0;i < n; i++)
+            for (int i = 0; i < n; i++)
             {
                 Console.Write("<" + (i + 1) + ">");
                 string s1 = Console.ReadLine();
                 arr[i] = Int32.Parse(s1);
+            }
+        }
+        // swaps the elements at index x with thw element at index y
 
 
         }
