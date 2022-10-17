67ef581ded42ac7ae1de8fdd9d510e7d4a5805b5
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2022-10-17 17:38:50 +0700

    menambahkan j now contains the index of the last element in the sorted list

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -86,6 +86,11 @@ namespace QuickSort
                 if (i < j) //if the greater element is on the left of the element
                 {
                     //swap the element at index i whit the element at index j
+                    swap(i, j);
+                    mov_count++;
+                }
+            }
+            //j now contains the index of the last element in the sorted list 
 
 
 
